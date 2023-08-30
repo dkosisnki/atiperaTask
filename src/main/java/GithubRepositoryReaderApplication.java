5dfa244af2018160707d72473e2278b1802b5156
Damian <d.kosinski13@wp.pl>
2023-08-30 15:40:49 +0200

    Init commit

---
+++ src/main/java/GithubRepositoryReaderApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoryReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepositoryReaderApplication.class);
+    }
+
+}
